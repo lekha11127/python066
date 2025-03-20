78a5efb04a8ce6d8651a7dcd7f0babf025f50ec0
lekha11127 <parlagorlas@gmail.com>
2025-03-20 23:18:34 +0530

    Add files via upload

---
+++ perameter.py
@@ -0,0 +1,6 @@
+length=int(input("enter the length of rectangle:"))
+breadth=int(input("enter the breadth of rectangle:"))
+area=length*breadth
+perameter=2*(length+breadth)
+print("the area of rectangle is:",area)
+print("the perameter of rectangle is:",perameter)
---
+++ swapping.py
@@ -0,0 +1,4 @@
+a,b=7,8
+print("before swapping:",a,b)
+a,b=b,a
+print("after swapping:",a,b)
